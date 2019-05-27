00421a8a2a0d7a2702b3d414844d0cd431dafa3c
screemix <43718473+screemix@users.noreply.github.com>
2019-05-28 00:31:12 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,45 +8,46 @@ class operation{
 }
 
 class expression_tree{
-    public int value;
-    public int height;
-    public int fitness;
-    public operation head;
-    public expression_tree left;
-    public expression_tree right;
+    public int value; //value of the expression
+//    public int height; //height of the tree
+    public int fitness; //value of fitness function
+    public operation head; //operator which is situated on the top of the tree
+    public expression_tree left; //left part of the tree
+    public expression_tree right; //right part of the tree
+
+    /* function for calculating the value of expression*/
     public int calculate_value(){
-        if (head==null){
-            return value;
-        }
-        else {
-            if (head.equals('+')){
-                return left.calculate_value()+right.calculate_value();
+            if ((head.operator=='@')){
+                return value;
             }
-            else if (head.equals('-')){
+            else if (head.operator=='+'){
+                value = left.calculate_value()+right.calculate_value();
+                return value;
+            }
+            else if (head.operator=='-'){
                 return left.calculate_value()-right.calculate_value();
             }
-            else if (head.equals('*')){
+            else if (head.operator=='*'){
                 return left.calculate_value()*right.calculate_value();
             }
             else{
                 return left.calculate_value()/right.calculate_value();
             }
+
         }
-    }
     public int calculate_fintess(){
         fitness = Math.abs(35-value);
         return fitness;
     }
-    public expression_tree(operation a, int b){
+    public expression_tree(operation a){
         head = a;
-        height = b;
     };
     public expression_tree(int val){
         value = val;
     }
     public expression_tree(){}
-
     }
+
 public class Main {
     static expression_tree[] generation = new expression_tree[32];
     static operation four_operations(){
@@ -87,42 +88,29 @@ public class Main {
             return new_one;
         }
         else{
-            return null;
+            operation new_one = new operation('/');
+            return new_one;
+        }
+    }
+    static expression_tree create_subtree(int height){
+        if (height == 0){
+            int val = (int)(Math.random()*10);
+            expression_tree new_one = new expression_tree(val);
+            new_one.head = new operation('@');
+            return new_one;
+        }
+        else {
+            int new_height = (int)(Math.random()*10%(height));
+            expression_tree new_one = new expression_tree(four_operations());
+            new_one.left = create_subtree(new_height);
+            new_one.right = create_subtree(new_height);
+            return new_one;
         }
     }
     static void first_generation(){
         for (int i = 0; i<32; i++){
-            expression_tree new_one= new expression_tree(four_operations(), (int)Math.random()*10);
-            expression_tree cur = new_one;
-            for (int j = 0; j<new_one.height-1; j++){
-                expression_tree second = new expression_tree(five_operations(), (int)Math.random()*10);
-                cur.left = second;
-                cur = second;
-                if (cur.head==null){
-                    cur.left = new expression_tree((int)Math.random()*10);
-                    cur.right = new expression_tree((int)Math.random()*10);
-                    break;
-                }
-            }
-            if (new_one.height == 6){
-                cur.left = new expression_tree((int)Math.random()*10);
-                cur.right = new expression_tree((int)Math.random()*10);
-            }
-            cur = new_one;
-            for (int j = 0; j<new_one.height-1; j++){
-                expression_tree second = new expression_tree(five_operations(), (int)Math.random()*10);
-                cur.right = second;
-                cur = second;
-                if (cur.head==null){
-                    cur.left = new expression_tree((int)Math.random()*10);
-                    cur.right = new expression_tree((int)Math.random()*10);
-                    break;
-                }
-            }
-            if (new_one.height == 6){
-                cur.left = new expression_tree((int)Math.random()*10);
-                cur.right = new expression_tree((int)Math.random()*10);
-            }
+            int height = (int)(Math.random()*10%6);
+            expression_tree new_one= create_subtree(height);
             generation[i] = new_one;
 
         }
@@ -151,7 +139,7 @@ public class Main {
     }
     static expression_tree mutate(expression_tree tree){
         expression_tree cur = tree;
-        while (cur.head!=null){
+        while (!(cur.head.operator=='@')){
             int side = (int)(Math.round(Math.random()));
             int change = (int)(Math.round(Math.random()));
             if (change==1){
@@ -164,29 +152,22 @@ public class Main {
                 cur = cur.right;
             }
         }
-        int side = (int)(Math.round(Math.random()));
-        if (side ==0){
-            cur.left.value = (int)(Math.random()*10);
-        }
-        else{
-            cur.right.value = (int)(Math.random()*10);
-        }
+        cur.value = (int)(Math.random()*10);
         return tree;
     }
     static void sort(){
         for (int i = 0; i<32; i++){
             for (int j = 1; j<32; j++){
-                if(generation[j].fitness>generation[j-1].fitness){
-                    expression_tree temp = generation[i];
-                    generation[i] = generation[j];
-                    generation[j] = temp;
+                if(generation[j].fitness<generation[j-1].fitness){
+                    expression_tree temp = generation[j];
+                    generation[j] = generation[j-1];
+                    generation[j-1] = temp;
 
                 }
             }
         }
     }
     static void selecton(){
-        sort();
         for (int i = 16, j = 0; i<32; i++, j++){
             generation[i] = crosover(generation[j],generation[j+1]);
         }
@@ -194,12 +175,12 @@ public class Main {
 
     public static void main(String[] args) {
         first_generation();
-        for (int i = 0; i<50; i++){
+        for (int i = 0; i<200; i++){
             fitness();
             sort();
             selecton();
         }
         sort();
-        System.out.println(generation[0]);
+        System.out.println(generation[0].value);
     }
 }
